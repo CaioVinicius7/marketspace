ffc9b6d5e62cee99b5ebc55de8ff00c3222bba62
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-03 13:11:14 -0300

    feat: :wrench: Customização o tema default do native base

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { NativeBaseProvider, Center } from "native-base";
 
+import { THEME } from "./src/theme";
+
 export default function App() {
 	return (
-		<NativeBaseProvider>
+		<NativeBaseProvider theme={THEME}>
 			<Center flex={1}>Hello world</Center>
 		</NativeBaseProvider>
 	);
---
+++ src/theme/index.ts
@@ -0,0 +1,34 @@
+import { extendTheme } from "native-base";
+
+export const THEME = extendTheme({
+	colors: {
+		blue: {
+			400: "#647AC7",
+			700: "#364D9D"
+		},
+		gray: {
+			700: "#1A181B",
+			600: "#3E3A40",
+			500: "#5F5B62",
+			400: "#9F9BA1",
+			300: "#D9D8DA",
+			200: "#EDECEE",
+			100: "#F7F7F8"
+		},
+		red: {
+			400: "##EE7979"
+		},
+		white: "#FFFFFF"
+	},
+	fontSizes: {
+		xs: 12,
+		sm: 14,
+		md: 16,
+		lg: 18,
+		xl: 20
+	},
+	sizes: {
+		14: 56,
+		33: 148
+	}
+});
