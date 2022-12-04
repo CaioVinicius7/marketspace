b956153c25bb856bcd0890db84de3616543b8396
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-04 16:10:13 -0300

    feat: :construction: Formulário de cadastro e rodapé da página SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,8 +1,20 @@
 import { Center, Text, ScrollView, VStack, Heading } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
 import LogoSvg from "@assets/logo.svg";
 
 export function SignUp() {
+	const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+	function handleGoBack() {
+		navigation.goBack();
+	}
+
 	return (
 		<ScrollView flex={1}>
 			<VStack py={9} px={12} safeArea>
@@ -17,6 +29,27 @@ export function SignUp() {
 						Crie sua conta e use o espaço para comprar {"\n"} itens variados e
 						vender seus produtos
 					</Text>
+				</Center>
+
+				<Center mt={6}>
+					<Input placeholder="Nome" />
+					<Input placeholder="E-mail" keyboardType="email-address" mt={6} />
+					<Input placeholder="Telefone" keyboardType="phone-pad" mt={6} />
+					<Input placeholder="Senha" passwordInput mt={6} />
+					<Input placeholder="Confirmar senha" passwordInput mt={6} />
+
+					<Button title="Criar" variant="tertiary" mt={6} />
+				</Center>
+
+				<Center mt={12}>
+					<Text>Já tem uma conta?</Text>
+
+					<Button
+						title="Ir para o login"
+						variant="secondary"
+						mt={4}
+						onPress={handleGoBack}
+					/>
 				</Center>
 			</VStack>
 		</ScrollView>
