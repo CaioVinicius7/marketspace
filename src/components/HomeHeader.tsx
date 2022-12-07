3867da0b96498d1b84d598e06c993476dce5d577
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-07 20:33:28 -0300

    feat: :lipstick: Componente HomeHeader

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,43 @@
+import { Heading, HStack, Icon, Text, useTheme, VStack } from "native-base";
+import { Plus } from "phosphor-react-native";
+
+import { Avatar } from "./Avatar";
+import { Button } from "./Button";
+
+import defaultUserPhotoImage from "@assets/userPhotoDefault.png";
+
+export function HomeHeader() {
+	const userPhoto = "https://github.com/caiovinicius7.png";
+
+	const { colors } = useTheme();
+
+	return (
+		<HStack alignItems="center">
+			<Avatar
+				size={12}
+				source={userPhoto ? { uri: userPhoto } : defaultUserPhotoImage}
+			/>
+
+			<VStack flex={1}>
+				<Text color="gray.700" fontSize="md">
+					Boas vindas,
+				</Text>
+
+				<Heading color="gray.700" fontSize="md" fontFamily="heading">
+					Caio
+				</Heading>
+			</VStack>
+
+			<Button
+				title="Criar anúncio"
+				variant="tertiary"
+				leftIcon={
+					<Icon
+						as={<Plus size={16} weight="bold" color={colors.gray[100]} />}
+					/>
+				}
+				flex={1}
+			/>
+		</HStack>
+	);
+}
