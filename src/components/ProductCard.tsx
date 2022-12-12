3492caa1a3b33443d538021052ed7043bd9613c5
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-12 17:41:37 -0300

    feat: :lipstick: Componente ProductCard

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,96 @@
+import {
+	Heading,
+	HStack,
+	Image,
+	Pressable,
+	Tag,
+	Text,
+	VStack
+} from "native-base";
+
+import { Avatar } from "./Avatar";
+
+interface ProductCardProps {
+	title: string;
+	value: string;
+	status: "used" | "new";
+	productImg: string;
+	vendorImg: string;
+}
+
+export function ProductCard({
+	title,
+	value,
+	status,
+	productImg,
+	vendorImg
+}: ProductCardProps) {
+	const productIsNew = status == "new";
+
+	function handleOnPress() {
+		console.log(`Selected the product ${title}`);
+	}
+
+	return (
+		<Pressable w="1/2" onPress={handleOnPress}>
+			<VStack>
+				<Image
+					source={{
+						uri: productImg
+					}}
+					alt="Product Image"
+					h={100}
+					w="full"
+					borderRadius="md"
+					resizeMode="cover"
+				/>
+
+				<HStack position="absolute" w="full" justifyContent="space-between">
+					<Avatar
+						size={6}
+						source={{
+							uri: vendorImg
+						}}
+						borderColor="gray.100"
+						borderWidth="1"
+						top={-4}
+						left={-4}
+					/>
+
+					<Tag
+						bgColor={productIsNew ? "blue.600" : "gray.600"}
+						borderRadius="full"
+						h={4}
+						px={2}
+						py={0.5}
+						mt={1}
+						mr={1}
+					>
+						<Text
+							fontSize="2xs"
+							fontFamily="heading"
+							textTransform="uppercase"
+							color="white"
+						>
+							{productIsNew ? "Novo" : "Usado"}
+						</Text>
+					</Tag>
+				</HStack>
+
+				<Text color="gray.600" lineHeight="2xl">
+					{title}
+				</Text>
+
+				<HStack alignItems="baseline">
+					<Heading color="gray.700" fontSize="xs">
+						R$
+					</Heading>
+
+					<Heading color="gray.700" fontSize="md" lineHeight="xs" pl={0.5}>
+						{value}
+					</Heading>
+				</HStack>
+			</VStack>
+		</Pressable>
+	);
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,7 @@ import {
 	Heading,
 	HStack,
 	Icon,
+	Image,
 	Pressable,
 	Text,
 	useTheme,
@@ -21,6 +22,7 @@ import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Input } from "@components/Input";
+import { ProductCard } from "@components/ProductCard";
 
 export function Home() {
 	const { colors } = useTheme();
@@ -99,6 +101,24 @@ export function Home() {
 					}
 				/>
 			</VStack>
+
+			<HStack flexWrap="wrap">
+				<ProductCard
+					title="Tênis bege"
+					value="59,90"
+					status="used"
+					productImg="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+					vendorImg="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+				/>
+
+				<ProductCard
+					title="Bicicleta"
+					value="120,00"
+					status="new"
+					productImg="https://images.unsplash.com/photo-1484920274317-87885fcbc504?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+					vendorImg="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+				/>
+			</HStack>
 		</VStack>
 	);
 }
