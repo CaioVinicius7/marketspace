505cb1ca8c266dacb4f52568922c569372bf674e
Caio Vinícius <caio1525pereira@gmail.com>
2022-12-18 19:03:15 -0300

    feat: :lipstick: Componente FilterBottomSheet

---
+++ src/components/FilterBottomSheet.tsx
@@ -0,0 +1,188 @@
+import { useMemo, useRef, Dispatch, SetStateAction } from "react";
+import {
+	Checkbox,
+	Heading,
+	HStack,
+	IconButton,
+	Switch,
+	Tag,
+	Text,
+	useTheme,
+	VStack
+} from "native-base";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { X } from "phosphor-react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Button } from "./Button";
+import { ScrollView } from "react-native-gesture-handler";
+import { Pressable } from "react-native";
+
+interface FilterBottomSheetProps {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export function FilterBottomSheet({
+	isOpen,
+	setIsOpen
+}: FilterBottomSheetProps) {
+	const { colors } = useTheme();
+
+	const bottomSheetRef = useRef<BottomSheet>(null);
+
+	const snapPoints = useMemo(() => [0.1, "65%"], []);
+
+	return (
+		<BottomSheet
+			index={isOpen ? 1 : 0}
+			ref={bottomSheetRef}
+			snapPoints={snapPoints}
+			enableHandlePanningGesture={false}
+		>
+			<VStack px={8} pt={6} flex={1}>
+				<HStack alignItems="center" justifyContent="space-between">
+					<Heading fontSize="xl">Filtrar anúncios</Heading>
+
+					<Pressable onPress={() => setIsOpen(false)}>
+						<X size={24} color={colors.gray[400]} />
+					</Pressable>
+				</HStack>
+
+				<ScrollView showsVerticalScrollIndicator={false}>
+					<Text color="gray.600" fontFamily="heading" fontSize="sm" mt={6}>
+						Condição
+					</Text>
+
+					<HStack>
+						<Tag
+							bgColor="blue.400"
+							borderRadius="full"
+							h={7}
+							px={4}
+							py={1}
+							mt={3}
+							mr={1}
+						>
+							<Text
+								fontSize="xs"
+								fontFamily="heading"
+								textTransform="uppercase"
+								color="white"
+							>
+								Novo
+							</Text>
+
+							<IconButton
+								bgColor="gray.200"
+								size="sm"
+								borderRadius="full"
+								p={0.4}
+								ml={1.5}
+								mr={-1.5}
+								_icon={{
+									as: MaterialCommunityIcons,
+									name: "window-close",
+									size: 3,
+									color: "blue.400"
+								}}
+							/>
+						</Tag>
+
+						<Tag
+							bgColor="gray.300"
+							borderRadius="full"
+							h={7}
+							px={4}
+							py={1}
+							mt={3}
+							mr={1}
+						>
+							<Text
+								fontSize="xs"
+								fontFamily="heading"
+								textTransform="uppercase"
+								color="gray.500"
+							>
+								Usado
+							</Text>
+						</Tag>
+					</HStack>
+
+					<VStack alignItems="flex-start" mt={6}>
+						<Text color="gray.600" fontFamily="heading" fontSize="sm">
+							Aceita troca?
+						</Text>
+
+						<Switch size="lg" p={0} onChange={() => {}} />
+					</VStack>
+
+					<Text color="gray.600" fontFamily="heading" fontSize="sm" mt={6}>
+						Meios de pagamento aceitos
+					</Text>
+
+					<VStack mt={3} space={2}>
+						<Checkbox
+							_checked={{
+								bgColor: "blue.400",
+								borderColor: "blue.400"
+							}}
+							value="boleto"
+							onChange={() => {}}
+						>
+							<Text fontSize="md">Boleto</Text>
+						</Checkbox>
+
+						<Checkbox
+							_checked={{
+								bgColor: "blue.400",
+								borderColor: "blue.400"
+							}}
+							value="pix"
+							onChange={() => {}}
+						>
+							<Text fontSize="md">Pix</Text>
+						</Checkbox>
+
+						<Checkbox
+							_checked={{
+								bgColor: "blue.400",
+								borderColor: "blue.400"
+							}}
+							value="Dinheiro"
+							onChange={() => {}}
+						>
+							<Text fontSize="md">Dinheiro</Text>
+						</Checkbox>
+
+						<Checkbox
+							_checked={{
+								bgColor: "blue.400",
+								borderColor: "blue.400"
+							}}
+							value="Cartão de Crédito"
+							onChange={() => {}}
+						>
+							<Text fontSize="md">Cartão de Crédito</Text>
+						</Checkbox>
+
+						<Checkbox
+							_checked={{
+								bgColor: "blue.400",
+								borderColor: "blue.400"
+							}}
+							value="Depósito Bancário"
+							onChange={() => {}}
+						>
+							<Text fontSize="md">Depósito Bancário</Text>
+						</Checkbox>
+					</VStack>
+				</ScrollView>
+
+				<HStack justifyContent="space-between" mt={8} mb={8}>
+					<Button title="Resetar filtros" variant="secondary" w="47.5%" />
+					<Button title="Aplicar filtros" variant="tertiary" w="47.5%" />
+				</HStack>
+			</VStack>
+		</BottomSheet>
+	);
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
 	Button as NativeBaseButton,
 	Divider,
@@ -17,14 +18,18 @@ import {
 	Sliders,
 	Tag
 } from "phosphor-react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Input } from "@components/Input";
 import { ProductCard } from "@components/ProductCard";
+import { FilterBottomSheet } from "@components/FilterBottomSheet";
 
 export function Home() {
+	const [filterOptionsIsOpen, setFilterOptionsIsOpen] = useState(false);
+
 	const { colors } = useTheme();
 
 	const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -34,96 +39,103 @@ export function Home() {
 	}
 
 	return (
-		<VStack flex={1} pt={4} px={6} space={8} safeArea>
-			<HomeHeader />
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<VStack flex={1} pt={4} px={6} space={8} safeArea>
+				<HomeHeader />
 
-			<VStack>
-				<Text color="gray.500">Seus produtos anunciados para venda</Text>
+				<VStack>
+					<Text color="gray.500">Seus produtos anunciados para venda</Text>
 
-				<HStack
-					bgColor="#dfe1ea"
-					px={4}
-					py={3}
-					mt={3}
-					borderRadius="md"
-					alignItems="center"
-				>
-					<Tag size={22} weight="bold" color={colors.blue[700]} />
-
-					<VStack flex={1} ml={4}>
-						<Heading color="gray.600" fontSize="xl">
-							4
-						</Heading>
-
-						<Text color="gray.600" fontSize="xs">
-							anúncios ativos
-						</Text>
-					</VStack>
-
-					<NativeBaseButton
-						onPress={handleGoToLoggedUserAds}
-						variant="unstyled"
-						rightIcon={
-							<Icon
-								as={
-									<ArrowRight
-										size={16}
-										weight="bold"
-										color={colors.blue[700]}
-									/>
-								}
-							/>
-						}
+					<HStack
+						bgColor="#dfe1ea"
+						px={4}
+						py={3}
+						mt={3}
+						borderRadius="md"
+						alignItems="center"
 					>
-						<Text fontSize="xs" color="blue.700" pr={2} fontWeight="bold">
-							Meus anúncios
-						</Text>
-					</NativeBaseButton>
-				</HStack>
-			</VStack>
+						<Tag size={22} weight="bold" color={colors.blue[700]} />
 
-			<VStack>
-				<Text color="gray.500">Compre produtos variados</Text>
+						<VStack flex={1} ml={4}>
+							<Heading color="gray.600" fontSize="xl">
+								4
+							</Heading>
 
-				<Input
-					placeholder="Buscar anúncio"
-					mt={3}
-					InputRightElement={
-						<HStack pr={4}>
-							<Pressable onPress={() => console.log("Search")}>
-								<MagnifyingGlass size={20} />
-							</Pressable>
-							<Divider orientation="vertical" h={5} bg="gray.400" mx={3} />
-							<Pressable onPress={() => console.log("Filter")}>
-								<Sliders size={20} />
-							</Pressable>
-						</HStack>
-					}
+							<Text color="gray.600" fontSize="xs">
+								anúncios ativos
+							</Text>
+						</VStack>
+
+						<NativeBaseButton
+							onPress={handleGoToLoggedUserAds}
+							variant="unstyled"
+							rightIcon={
+								<Icon
+									as={
+										<ArrowRight
+											size={16}
+											weight="bold"
+											color={colors.blue[700]}
+										/>
+									}
+								/>
+							}
+						>
+							<Text fontSize="xs" color="blue.700" pr={2} fontWeight="bold">
+								Meus anúncios
+							</Text>
+						</NativeBaseButton>
+					</HStack>
+				</VStack>
+
+				<VStack>
+					<Text color="gray.500">Compre produtos variados</Text>
+
+					<Input
+						placeholder="Buscar anúncio"
+						mt={3}
+						InputRightElement={
+							<HStack pr={4}>
+								<Pressable onPress={() => console.log("Search")}>
+									<MagnifyingGlass size={20} />
+								</Pressable>
+								<Divider orientation="vertical" h={5} bg="gray.400" mx={3} />
+								<Pressable onPress={() => setFilterOptionsIsOpen(true)}>
+									<Sliders size={20} />
+								</Pressable>
+							</HStack>
+						}
+					/>
+				</VStack>
+
+				<FlatList
+					data={["1", "2", "3", "4", "5", "6"]}
+					keyExtractor={(item) => item}
+					numColumns={2}
+					showsVerticalScrollIndicator={false}
+					renderItem={({ item }) => (
+						<ProductCard
+							title="Tênis bege"
+							value="59,90"
+							status="used"
+							productImg="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
+							vendorImg="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+						/>
+					)}
+					columnWrapperStyle={{
+						justifyContent: "space-between"
+					}}
+					ItemSeparatorComponent={() => <Divider opacity={0} h={6} />}
+					contentContainerStyle={{
+						paddingBottom: 64
+					}}
 				/>
 			</VStack>
 
-			<FlatList
-				data={["1", "2", "3", "4", "5", "6"]}
-				keyExtractor={(item) => item}
-				numColumns={2}
-				showsVerticalScrollIndicator={false}
-				renderItem={({ item }) => (
-					<ProductCard
-						title="Tênis bege"
-						value="59,90"
-						status="used"
-						productImg="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80"
-						vendorImg="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-					/>
-				)}
-				columnWrapperStyle={{
-					justifyContent: "space-between"
-				}}
-				ItemSeparatorComponent={() => <Divider opacity={0} h={6} />}
-				contentContainerStyle={{
-					paddingBottom: 64
-				}}
+			<FilterBottomSheet
+				isOpen={filterOptionsIsOpen}
+				setIsOpen={setFilterOptionsIsOpen}
 			/>
-		</VStack>
+		</GestureHandlerRootView>
 	);
 }
